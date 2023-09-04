36120030c6c601a32d9f8a4545d22226cc2a2cb9
Serhiі Stupnytskyi <stupns@macPro.attlocal.net>
2023-09-04 15:49:01 -0400

    change logic syntax

--- mynotes/src/pages/NotePage.js
+++ mynotes/src/pages/NotePage.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import notes from '../assets/data';
+import React, {useState, useEffect} from 'react';
+//import notes from '../assets/data';
 import { Link, useParams } from 'react-router-dom'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 function NotePage() {
   const { id } = useParams();
-  const noteId = parseInt(id);
-  const note = notes.find(note => note.id === noteId);
+  let noteId = parseInt(id);
+  
+
+  let [note, setNote] = useState(null)
+
+  useEffect(() => {
+    getNote()
+  }, [noteId])
+
+  //const note = notes.find(note => note.id === noteId);
+
+  let getNote = async () => {
+    let response = await fetch(`http://localhost:5000/notes/${noteId}`)
+    let data = await response.json()
+    setNote(data)
+  }
 
   return (
     <div className='note'>
